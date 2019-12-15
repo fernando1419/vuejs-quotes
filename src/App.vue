4f9d81bfe6a2a451d2d56213a1116fa5290d9bf8
Fernando Lamas <lamas.fernando1419@gmail.com>
2019-12-15 20:52:12 -0300

    Moving forward with Quote component code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
+    <div class="container">
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
 
     export default {
         data: function() {
             return {
-                quoutes: [
-                    'One testing element'
+                quotes: [
+                    'One testing element',
+                    'Second Element',
+                    'Third element!',
+                    'Fourth element!',
+                    'The last element!'
                 ],
                 maxQuotes: 10
             }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
         }
     }
-
 </script>
 
 <style>
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,13 +1,19 @@
 <template>
-  
+    <div class="row">
+        <app-quote v-for="(quote, index) in quotes" :key="index">{{ quote }}</app-quote>
+    </div>
 </template>
 
 <script>
-    export default {
+    import Quote from './Quote.vue'
 
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote: Quote
+        },
     }
 </script>
 
 <style>
-
 </style>
